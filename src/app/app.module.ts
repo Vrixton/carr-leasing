1db7879edec7cd317e56039951d4cb53b6e93403
Victor Villavicencio <victor.villavicencio.10@gmail.com>
2020-06-21 17:59:15 -0500

    carousel in home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    CarouselModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
